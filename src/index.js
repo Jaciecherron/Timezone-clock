616336b7b6c7e96b5fb6f635cb33203f993e0df9
Jaciecherron <jaciecherron@gmail.com>
2024-10-16 15:04:52 -0600

    added second city and timezone

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 let londonElement = document.querySelector ("#london");
 let londonDateElement = londonElement.querySelector(".date");
 let londonTimeElement = londonElement.querySelector(".time");
-let londonTime = moment();
+let londonTime = moment().tz("Europe/London");
 londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY")
-londonTimeElement.innerHTML =  `${londonTime.format("h:mm:ss")} <small>${londonTime.format("A")}</small>`;
+londonTimeElement.innerHTML =  `${londonTime.format("hh:mm:ss")} <small>${londonTime.format("A")}</small>`;
+
+let denverElement = document.querySelector("#denver");
+let denverDateElement = denverElement.querySelector(".date");
+let denverTimeElement = denverElement.querySelector(".time");
+let denverTime = moment().tz("America/Denver");
+denverDateElement.innerHTML = denverTime.format("MMMM Do YYYY");
+denverTimeElement.innerHTML = `${denverTime.format(
+  "hh:mm:ss"
+)} <small>${denverTime.format("A")}</small>`;
