7982327242d0243eddc3b479c317fa1885a98dc5
Jaciecherron <jaciecherron@gmail.com>
2024-10-16 14:58:32 -0600

    added time format

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 let londonElement = document.querySelector ("#london");
 let londonDateElement = londonElement.querySelector(".date");
 let londonTimeElement = londonElement.querySelector(".time");
+let londonTime = moment();
+londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY")
+londonTimeElement.innerHTML =  `${londonTime.format("h:mm:ss")} <small>${londonTime.format("A")}</small>`;
