0b03fc9f5bef465251f96980dd438f741a7837a6
Jaciecherron <jaciecherron@gmail.com>
2024-10-16 15:14:36 -0600

    cleaned up HTML and JS functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
+function updateTime() {
 let londonElement = document.querySelector ("#london");
 let londonDateElement = londonElement.querySelector(".date");
 let londonTimeElement = londonElement.querySelector(".time");
 let londonTime = moment().tz("Europe/London");
 londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY")
 londonTimeElement.innerHTML =  `${londonTime.format("hh:mm:ss")} <small>${londonTime.format("A")}</small>`;
-
 let denverElement = document.querySelector("#denver");
 let denverDateElement = denverElement.querySelector(".date");
 let denverTimeElement = denverElement.querySelector(".time");
@@ -13,3 +13,7 @@ denverDateElement.innerHTML = denverTime.format("MMMM Do YYYY");
 denverTimeElement.innerHTML = `${denverTime.format(
   "hh:mm:ss"
 )} <small>${denverTime.format("A")}</small>`;
+}
+
+updateTime ();
+setInterval(updateTime, 1000);
