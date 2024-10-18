fc4d6fa45f0180ff430a0523346dd2afb5726241
Jaciecherron <jaciecherron@gmail.com>
2024-10-18 16:13:03 -0600

    fixed elements to show only when city not selected

--- src/index.js
+++ src/index.js
@@ -1,22 +1,25 @@
 function updateTime() {
 let londonElement = document.querySelector ("#london");
+if (londonElement){
 let londonDateElement = londonElement.querySelector(".date");
 let londonTimeElement = londonElement.querySelector(".time");
 let londonTime = moment().tz("Europe/London");
 londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY")
-londonTimeElement.innerHTML =  `${londonTime.format("hh:mm:ss")} <small>${londonTime.format("A")}</small>`;
+londonTimeElement.innerHTML =  `${londonTime.format("hh:mm:ss")} <small>${londonTime.format("A")}</small>`};
 let denverElement = document.querySelector("#denver");
+if (denverElement){
 let denverDateElement = denverElement.querySelector(".date");
 let denverTimeElement = denverElement.querySelector(".time");
 let denverTime = moment().tz("America/Denver");
 denverDateElement.innerHTML = denverTime.format("MMMM Do YYYY");
 denverTimeElement.innerHTML = `${denverTime.format(
   "hh:mm:ss"
-)} <small>${denverTime.format("A")}</small>`;
-}
+)} <small>${denverTime.format("A")}</small>`};
+};
 
 function updateCity(event){
   let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
